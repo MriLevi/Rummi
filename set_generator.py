bda1e07720cb05b4de07a63dae162beb16764a1a
MriLevi <levi.verhoef@student.hu.nl>
2021-06-05 16:01:50 +0200

    added generate_valid_runs and generate_valid_groups to set_generator.py, which generate all possible groups and runs

--- set_generator.py
+++ set_generator.py
@@ -1,18 +1,20 @@
+import itertools
 import numpy as np
-from itertools import combinations
 from collections import defaultdict
 
 class SetGenerator:
 
-    def __init__(self, numbers=13, colors=4, jokers=2, combi_len=3, tile_sets=2):
+    def __init__(self, numbers=13, colors=4, jokers=2, combi_len=3, tile_sets=2, minimal_startscore=30):
             self.numbers = numbers
             self.colors = colors
             self.jokers = jokers
             self.combi_len = combi_len
             self.tile_sets = tile_sets
             self.tiles = self.generate_tiles()
-            self.runs = set()
-            self.groups = set()
+            self.tilecount = (colors*numbers)+jokers
+            self.minimal_startscore = minimal_startscore
+            self.runs = self.generate_valid_runs()
+            self.groups = self.generate_valid_groups()
             self.sets = set()
             #self.generate_sets()
 
@@ -21,11 +23,41 @@ class SetGenerator:
         for tile_set in range(1, self.tile_sets+1):
             for number in range(1, self.numbers+1):
                 for color in range(1, self.colors+1):
-                    tiles[color].append(number)
+                    tiles[f'{color}_{tile_set}'].append(number)
         for joker in range(1, self.jokers+1):
             tiles['jokers'].append(joker)
-        print(tiles)
+        return tiles
+
+    def generate_valid_groups(self):
+        groups = []
+        for i in range(1,14):
+            groups.append((i,i,i))
+            groups.append((i,i,i,i))
+        return groups
+
+    def generate_valid_runs(self):
+        runs = []
+        for value in self.tiles.values():
+            for i in range(3, 14):
+                for item in itertools.combinations(value, i):
+                    all_1_diff = True
+                    for j in range(1, len(item)):
+                        if item[j] - item[j-1] != 1:
+                            all_1_diff = False
+                    if all_1_diff:
+                        runs.append(item)
+        return runs
+
+
+    def generate_possible_sets(self):
+        print('hoi')
+
 
 
 
 sg = SetGenerator()
+runs = sg.generate_valid_groups()
+groups = sg.generate_valid_runs()
+
+print(runs)
+print(groups)
