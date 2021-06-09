b29e4abbe7a0b6b1dce6f2c5906f8958d8ee1a07
MriLevi <74299880+MriLevi@users.noreply.github.com>
2021-06-09 20:13:48 +0200

    added pretty print function to show rack in a player-friendly way. added function to determine starting player

--- rummi.py
+++ rummi.py
@@ -1,5 +1,70 @@
-from set_generator import SetGenerator
+from game_engine import RummikubGame
+import set_generator
+import solve_tiles
+from collections import defaultdict
 
+def text_gui(query, *answers):
+    while True:
+        print(query)
+        inp = input()
+        answer = inp.lower() if inp.lower() in [str(answer) for answer in answers] else None
+        if answer is not None:
+            return answer
+        else:
+            print('Try again, that input was not valid')
 
+def rack_pretty_print(rack):
+    black, yellow, red, cyan, jokers = [], [], [], [], []
+    for key, value in rack.items():
 
-def start_of_game():
+        if 'black' in key:
+            if len(value) == 1:
+                black.append(value[0])
+            else:
+                for i in value:
+                    black.append(i)
+        if 'yellow' in key:
+            if len(value) == 1:
+                yellow.append(value[0])
+            else:
+                for i in value:
+                    yellow.append(i)
+        if 'red' in key:
+            if len(value) == 1:
+                red.append(value[0])
+            else:
+                for i in value:
+                    red.append(i)
+        if 'cyan' in key:
+            if len(value) == 1:
+                cyan.append(value[0])
+            else:
+                for i in value:
+                    cyan.append(i)
+        if 'jokers' in key:
+            if len(value) == 1:
+                jokers.append(value[0])
+            else:
+                for i in value:
+                    jokers.append(i)
+    print(f'Je hebt dit op je bord:\n Zwart: {sorted(black)}, Geel: {sorted(yellow)}, Rood: {sorted(red)}, Cyaan: {sorted(cyan)}, jokers: {sorted(jokers)}')
+
+def main():
+    rummi = RummikubGame()
+    playerrack = defaultdict(list)
+    playerrack = rummi.draw_tile(playerrack, tile_amount=14)
+    computerrack = defaultdict(list)
+    computerrack = rummi.draw_tile(computerrack, tile_amount=14)
+    playerfirsttile = next(iter(playerrack))
+    computerfirsttile = next(iter(computerrack))
+
+    print('Welkom bij rummikub!')
+    print(f'De computers eerste tegel is:{computerrack[computerfirsttile][0]}')
+    print(f'Jouw eerste tegel is: {playerrack[playerfirsttile][0]}')
+    if computerrack[computerfirsttile][0] > playerrack[playerfirsttile][0]:
+        print('De computer begint!')
+    else:
+        print('Jij begint!')
+    rack_pretty_print(playerrack)
+
+main()
