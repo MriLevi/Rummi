aef2ba2fb16cfcc183f474e52096428b102a27e6
MriLevi <74299880+MriLevi@users.noreply.github.com>
2021-06-11 23:06:26 +0200

    added a method to sort the values in dict

--- rummi.py
+++ rummi.py
@@ -3,13 +3,19 @@ from collections import defaultdict
 from console import Console
 
 
+def sort_defaultdict(dict):
+    for key, value in dict.items():
+        newvalue = sorted(value)
+        dict[key] = newvalue
+    return dict
+
 def main():
     game = RummikubGame()
     con = Console()
     playerrack = defaultdict(list)
-    playerrack = game.draw_tile(playerrack, tile_amount=14)
+    playerrack = sort_defaultdict(game.draw_tile(playerrack, tile_amount=14))
     computerrack = defaultdict(list)
-    computerrack = game.draw_tile(computerrack, tile_amount=14)
+    computerrack = sort_defaultdict(game.draw_tile(computerrack, tile_amount=14))
 
     print('Welcome to Rummikub!')
     playerstarts = game.select_starting_player()
